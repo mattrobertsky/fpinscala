33f2bc8055105688a7ff4ef186b906e74ebcaaf5
Matt Roberts <matt.roberts2@digital.hmrc.gov.uk>
2017-12-01 16:36:45 +0000

    Chapter 3, up to exercise 4.

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -50,13 +50,34 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = {
+    l match {
+      case Cons(x, xs) => xs
+      case _ => Nil
+    }
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = {
+    l match {
+      case Cons(x, xs) => Cons(h, xs)
+      case _=> Cons(h, Nil)
+    }
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    l match {
+      case Cons(x, Nil) => Cons(x, Nil)
+      case Cons(x, xs) if n == 0 => Cons(x, xs)
+      case Cons(x, xs) => drop(xs, n-1)
+    }
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = {
+    l match {
+      case Cons(x, xs) if (f(x)) => dropWhile(xs, f)
+      case _ => l
+    }
+  }
 
   def init[A](l: List[A]): List[A] = ???
 
